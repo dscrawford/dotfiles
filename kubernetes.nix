c8d2f19fd3477eff79d7edc4887836520055e56f
Daniel Crawford <daniel.sc.crawford@gmail.com>
2026-02-13 13:37:51 -0800

    Setting this back

--- kubernetes.nix
+++ kubernetes.nix
@@ -17,7 +17,7 @@ in
 
   services.kubernetes = {
     roles = if isMaster then [ "master" "node" ] else [ "node" ];
-    masterAddress = kubeMasterHostname;
+    masterAddress = if isMaster then "127.0.0.1" else masterIP;
     apiserverAddress = "https://${kubeMasterHostname}:${toString kubeMasterAPIServerPort}";
     easyCerts = true;
 
