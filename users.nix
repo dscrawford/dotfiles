d47d910ef49f9c549a41c68001266f3a110d7d04
Daniel Crawford <daniel.sc.crawford@gmail.com>
2026-02-15 22:54:12 -0800

    Add new SSH key

--- users.nix
+++ users.nix
@@ -9,6 +9,7 @@
     ];
     openssh.authorizedKeys.keys = [
       "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCZpsvf05hWJxKDmb5i1z8WLCo+paq4duftOhSDq0DAYrW0MguieW7LxgnuVvfy63N3rNjENPDXb/rUu85r9Mky4dIbRMrgGf+tTz5WM43TjpSFVkZDxeDGGF6fFGcv/gQGrDBLZhYooQp9Ip0Bb2LWYhxcYpQZumLiRxgRevcMQKZVvpmXj5fcq1OvokisFgqgudtl/qzolmHOTl/svTVqUVh58I2gBWp6PqHBTlviKj9MfoMm4V5uAcVfyZpmbhUDcBVWn44pUzDnJELSfG8rTopBYw7XTRpCGciGaG2o4uRt+0fSXHmOO+OQrHOzDHJMcj4lgB3AozvDTHr9tySg29WlDHVr7uUS81rSa30+gSYl/OtkKx9aO7GDoZ/eJyi48wOc4QSshAbpl6sc6LCnya2zEDBewxkgMCHfURWmfl0esAL2FYvetmtsig6RI03yMo2lwTRbvqK8A0bTybw0XAZb8quxQ/uW09yIa+kfe4i3nVKuQCETc71hXhGhQuk= daniel@nixos"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFhfSPSaZFyOzSYPzKPJv21QcBlQKIBnrhNjIJjp8pOZ"
     ];
     home = "/home/host";
   };
