a18abc4370b45eed9a3562a5559cbd056e6df4f2
Daniel Crawford <daniel.sc.crawford@gmail.com>
2026-02-15 21:12:46 -0800

    Re-organize local.nix file

--- hosts/local/local.nix
+++ hosts/local/local.nix
@@ -1,39 +1,81 @@
-# hosts/local/local.nix
-# Desktop-specific configuration for local machine
+# Desktop configuration for local machine
 { config, lib, pkgs, ... }:
 
 {
-  # Boot configuration
-  boot.loader = {
-    efi = {
-      canTouchEfiVariables = true;
-      efiSysMountPoint = "/boot";
+  # === Boot ===
+  boot = {
+    loader = {
+      efi = {
+        canTouchEfiVariables = true;
+        efiSysMountPoint = "/boot";
+      };
+      grub = {
+        enable = true;
+        device = "nodev";
+        useOSProber = true;
+        efiSupport = true;
+        gfxmodeEfi = "1280x720";
+      };
     };
-    grub = {
-      enable = true;
-      device = "nodev";
-      useOSProber = true;
-      efiSupport = true;
-      gfxmodeEfi = "1280x720";
-    };
+    supportedFilesystems = [ "ntfs" ];
+    kernelModules = [ "uinput" ];
   };
-  
-  boot.supportedFilesystems = [ "ntfs" ];
-  boot.kernelModules = [ "uinput" ];
 
-  # Time zone and locale
+  # === Locale ===
   time.timeZone = "America/Los_Angeles";
   time.hardwareClockInLocalTime = true;
-
   i18n.defaultLocale = "en_US.UTF-8";
   console = {
     font = "Lat2-Terminus16";
     keyMap = "us";
   };
 
-  # X11 and Desktop Environment
+  # === Nixpkgs ===
+  nixpkgs.config = {
+    allowUnfree = true;
+    allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+      "nvidia-x11"
+      "nvidia-settings"
+      "nvidia-persistenced"
+      "steam-original"
+      "steam-unwrapped"
+    ];
+  };
+
+  # === Hardware ===
+  hardware = {
+    graphics = {
+      enable = true;
+      enable32Bit = true;
+    };
+    nvidia = {
+      modesetting.enable = true;
+      powerManagement.enable = false;
+      open = false;
+      nvidiaSettings = true;
+      package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+        version = "590.48.01";
+        sha256_64bit = "sha256-ueL4BpN4FDHMh/TNKRCeEz3Oy1ClDWto1LO/LWlr1ok=";
+        sha256_aarch64 = lib.fakeSha256;
+        openSha256 = "sha256-ZpuVZybW6CFN/gz9rx+UJvQ715FZnAOYfHn5jt5Z2C8=";
+        settingsSha256 = "sha256-ZpuVZybW6CFN/gz9rx+UJvQ715FZnAOYfHn5jt5Z2C8=";
+        persistencedSha256 = lib.fakeSha256;
+      };
+    };
+    bluetooth = {
+      enable = true;
+      settings.General = {
+        Enable = "Source,Sink,Media,Socket";
+        Disable = "Socket";
+      };
+    };
+    xpadneo.enable = true;
+  };
+
+  # === Display ===
   services.xserver = {
     enable = true;
+    videoDrivers = [ "nvidia" ];
     desktopManager = {
       xterm.enable = false;
       xfce = {
@@ -44,24 +86,12 @@
     };
     windowManager.i3 = {
       enable = true;
-      extraPackages = with pkgs; [
-        dmenu
-        i3status
-        i3lock
-      ];
+      extraPackages = with pkgs; [ dmenu i3status i3lock ];
     };
-    videoDrivers = [ "nvidia" ];
   };
-  
   services.displayManager.defaultSession = "xfce+i3";
 
-  # Desktop services
-  services.gnome.gnome-keyring.enable = true;
-  services.dbus.enable = true;
-  services.printing.enable = true;
-  services.blueman.enable = true;
-
-  # Audio
+  # === Audio ===
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
@@ -71,152 +101,106 @@
     jack.enable = true;
   };
 
-  # Graphics and NVIDIA
-  hardware.graphics = {
-    enable = true;
-    enable32Bit = true;
-  };
-
-  hardware.nvidia = {
-    modesetting.enable = true;
-    powerManagement.enable = false;
-    open = false;
-    nvidiaSettings = true;
-    package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-      version = "590.48.01";
-      sha256_64bit = "sha256-ueL4BpN4FDHMh/TNKRCeEz3Oy1ClDWto1LO/LWlr1ok=";
-      sha256_aarch64 = lib.fakeSha256;
-      openSha256 = "sha256-ZpuVZybW6CFN/gz9rx+UJvQ715FZnAOYfHn5jt5Z2C8=";
-      settingsSha256 = "sha256-ZpuVZybW6CFN/gz9rx+UJvQ715FZnAOYfHn5jt5Z2C8=";
-      persistencedSha256 = lib.fakeSha256;
+  # === Services ===
+  services = {
+    dbus.enable = true;
+    printing.enable = true;
+    blueman.enable = true;
+    flatpak.enable = true;
+    gnome.gnome-keyring.enable = true;
+    udev.extraRules = ''
+      KERNEL=="uinput", SUBSYSTEM=="misc", MODE="0660", GROUP="input"
+      KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="114d", ATTRS{idProduct}=="8a12", MODE="0660", TAG+="uaccess"
+      KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="0bb4", ATTRS{idProduct}=="2c87", MODE="0660", TAG+="uaccess"
+      KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="0bb4", ATTRS{idProduct}=="0306", MODE="0660", TAG+="uaccess"
+      KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="0bb4", ATTRS{idProduct}=="0309", MODE="0660", TAG+="uaccess"
+      KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="0bb4", ATTRS{idProduct}=="030a", MODE="0660", TAG+="uaccess"
+      KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="0bb4", ATTRS{idProduct}=="030b", MODE="0660", TAG+="uaccess"
+      KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="0bb4", ATTRS{idProduct}=="030c", MODE="0660", TAG+="uaccess"
+      KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="0bb4", ATTRS{idProduct}=="030e", MODE="0660", TAG+="uaccess"
+      KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="28de", ATTRS{idProduct}=="1043", MODE="0660", TAG+="uaccess"
+      KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="28de", ATTRS{idProduct}=="1142", MODE="0660", TAG+="uaccess"
+      KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="28de", ATTRS{idProduct}=="2000", MODE="0660", TAG+="uaccess"
+      KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="28de", ATTRS{idProduct}=="2010", MODE="0660", TAG+="uaccess"
+      KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="28de", ATTRS{idProduct}=="2011", MODE="0660", TAG+="uaccess"
+      KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="28de", ATTRS{idProduct}=="2012", MODE="0660", TAG+="uaccess"
+      KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="28de", ATTRS{idProduct}=="2021", MODE="0660", TAG+="uaccess"
+      KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="28de", ATTRS{idProduct}=="2022", MODE="0660", TAG+="uaccess"
+      KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="28de", ATTRS{idProduct}=="2050", MODE="0660", TAG+="uaccess"
+      KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="28de", ATTRS{idProduct}=="2101", MODE="0660", TAG+="uaccess"
+      KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="28de", ATTRS{idProduct}=="2102", MODE="0660", TAG+="uaccess"
+      KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="28de", ATTRS{idProduct}=="2150", MODE="0660", TAG+="uaccess"
+      KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="28de", ATTRS{idProduct}=="2300", MODE="0660", TAG+="uaccess"
+      KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="28de", ATTRS{idProduct}=="2301", MODE="0660", TAG+="uaccess"
+      SUBSYSTEM=="tty", ATTRS{idVendor}=="28de", ATTRS{idProduct}=="2102", MODE="0660", TAG+="uaccess"
+    '';
+    openvpn.servers.homeVPN = {
+      config = '' config /root/nixos/openvpn/homeVPN.ovpn '';
+      autoStart = false;
     };
   };
 
-  # Bluetooth
-  hardware.bluetooth = {
+  # === XDG ===
+  xdg.portal = {
     enable = true;
-    settings.General = {
-      Enable = "Source,Sink,Media,Socket";
-      Disable = "Socket";
+    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+  };
+
+  # === Programs ===
+  programs = {
+    dconf.enable = true;
+    gamemode.enable = true;
+    steam = {
+      enable = true;
+      package = pkgs.steam.override {
+        extraPkgs = pkgs: with pkgs; [ gamemode ];
+      };
+    };
+    alvr = {
+      enable = true;
+      openFirewall = true;
+      package = pkgs.alvr;
     };
   };
 
-  # Virtualization
+  # === Virtualization ===
   virtualisation.docker.enable = true;
 
-  # Gaming
-  programs.gamemode.enable = true;
-  hardware.xpadneo.enable = true;
-  
-  programs.steam = {
-    enable = true;
-    package = pkgs.steam.override {
-      extraPkgs = (pkgs: with pkgs; [
-        gamemode
-      ]);
-    };
+  # === Environment ===
+  environment = {
+    systemPackages = with pkgs; [
+      exfat
+      bluez
+      cudatoolkit
+      gtk3
+      (ffmpeg-full.override { withUnfree = true; withOpengl = true; })
+      v4l-utils
+      guvcview
+      gnome-keyring
+      libsecret
+      tree
+      findutils
+      gnugrep
+      gnused
+      gawk
+      util-linux
+    ];
+    variables.GSETTINGS_SCHEMA_DIR = "${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}/glib-2.0/schemas";
+    pathsToLink = [ "/libexec" ];
   };
 
-  programs.alvr = {
-    enable = true;
-    openFirewall = true;
-    package = pkgs.alvr;
-  };
+  # === Security ===
+  security.pam.services.login.enableGnomeKeyring = true;
 
-  # udev rules for gaming controllers and VR
-  services.udev.extraRules = ''
-    KERNEL=="uinput", SUBSYSTEM=="misc", MODE="0660", GROUP="input"
-    KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="114d", ATTRS{idProduct}=="8a12", MODE="0660", TAG+="uaccess"
-
-    KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="0bb4", ATTRS{idProduct}=="2c87", MODE="0660", TAG+="uaccess"
-    KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="0bb4", ATTRS{idProduct}=="0306", MODE="0660", TAG+="uaccess"
-    KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="0bb4", ATTRS{idProduct}=="0309", MODE="0660", TAG+="uaccess"
-    KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="0bb4", ATTRS{idProduct}=="030a", MODE="0660", TAG+="uaccess"
-    KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="0bb4", ATTRS{idProduct}=="030b", MODE="0660", TAG+="uaccess"
-    KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="0bb4", ATTRS{idProduct}=="030c", MODE="0660", TAG+="uaccess"
-    KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="0bb4", ATTRS{idProduct}=="030e", MODE="0660", TAG+="uaccess"
-
-    KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="28de", ATTRS{idProduct}=="1043", MODE="0660", TAG+="uaccess"
-    KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="28de", ATTRS{idProduct}=="1142", MODE="0660", TAG+="uaccess"
-    KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="28de", ATTRS{idProduct}=="2000", MODE="0660", TAG+="uaccess"
-    KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="28de", ATTRS{idProduct}=="2010", MODE="0660", TAG+="uaccess"
-    KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="28de", ATTRS{idProduct}=="2011", MODE="0660", TAG+="uaccess"
-    KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="28de", ATTRS{idProduct}=="2012", MODE="0660", TAG+="uaccess"
-    KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="28de", ATTRS{idProduct}=="2021", MODE="0660", TAG+="uaccess"
-    KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="28de", ATTRS{idProduct}=="2022", MODE="0660", TAG+="uaccess"
-    KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="28de", ATTRS{idProduct}=="2050", MODE="0660", TAG+="uaccess"
-    KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="28de", ATTRS{idProduct}=="2101", MODE="0660", TAG+="uaccess"
-    KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="28de", ATTRS{idProduct}=="2102", MODE="0660", TAG+="uaccess"
-    KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="28de", ATTRS{idProduct}=="2150", MODE="0660", TAG+="uaccess"
-    KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="28de", ATTRS{idProduct}=="2300", MODE="0660", TAG+="uaccess"
-    KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="28de", ATTRS{idProduct}=="2301", MODE="0660", TAG+="uaccess"
-
-    SUBSYSTEM=="tty", ATTRS{idVendor}=="28de", ATTRS{idProduct}=="2102", MODE="0660", TAG+="uaccess"
-  '';
-
-  # Flatpak support
-  services.flatpak.enable = true;
-  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
-  xdg.portal.enable = true;
-
-  # GSettings
-  programs.dconf.enable = true;
-  environment.variables = {
-    GSETTINGS_SCHEMA_DIR = "${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}/glib-2.0/schemas";
-  };
-
-  environment.pathsToLink = [ "/libexec" ];
-
-  # Desktop-specific packages
-  environment.systemPackages = with pkgs; [
-    exfat
-    bluez
-    cudatoolkit
-    gtk3
-    (ffmpeg-full.override { withUnfree = true; withOpengl = true; })
-    v4l-utils
-    guvcview
-    gnome-keyring
-    libsecret
-    tree
-    findutils
-    gnugrep
-    gnused
-    gawk
-    util-linux
-  ];
-
-  # User configuration
+  # === Users ===
   users.users.daniel = {
     isNormalUser = true;
     extraGroups = [ "wheel" "docker" "bluetooth" "input" ];
   };
 
-  # Security
-  security.pam.services.login.enableGnomeKeyring = true;
-  
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-  nixpkgs.config.allowUnfreePredicate = pkg:
-    builtins.elem (lib.getName pkg) [
-      "nvidia-x11"
-      "nvidia-settings"
-      "nvidia-persistenced"
-      "steam-original"
-      "steam-unwrapped"
-    ];
-
-  # Custom networking
+  # === Networking ===
   networking.hosts = {
     "192.168.0.2" = [ "node1" "api.kube" ];
     "192.168.0.4" = [ "node2" ];
-  };
-
-  # OpenVPN configuration (disabled by default)
-  services.openvpn.servers = {
-    homeVPN = {
-      config = '' config /root/nixos/openvpn/homeVPN.ovpn '';
-      autoStart = false;
-    };
   };
 }
