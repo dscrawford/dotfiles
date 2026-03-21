5c95247d9fa653e320441d82df8e926794fc3e81
Daniel Crawford <daniel.sc.crawford@gmail.com>
2026-03-21 12:49:35 -0700

    I'm not entirely sure what changed here

--- hosts/local/local.nix
+++ hosts/local/local.nix
@@ -50,7 +50,7 @@
     };
     nvidia = {
       modesetting.enable = true;
-      powerManagement.enable = false;      
+      powerManagement.enable = false;
       open = false;      
       nvidiaSettings = true;
       package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
